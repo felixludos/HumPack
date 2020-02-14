1cd81f0a988de0e6640089fb39c66ce5e888506f
Felix Leeb <felix.leeb@tuebingen.mpg.de>
2020-02-14 18:07:22 +0100

    added readme to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup
 from humpack import _lib_info as info
+
+with open('README.rst', 'r') as f:
+      README = f.read()
 
 setup(name=info.name,
       version=info.version,
       description=info.description,
+      long_description=README,
+      long_description_content_type="text/markdown",
       url=info.url,
       author=info.author,
       author_email=info.author_email,
